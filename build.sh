ba5136d7a04926d42ca401cb4d67707cd1db97e3
Prakhar <b118038@iiit-bh.ac.in>
2020-02-14 16:25:01 +0000

    edited gitignore

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+platforms=("windows/amd64" "windows/386" "darwin/amd64" "linux/386")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name="howto-"$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'$package
+    fi  
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o 'bin/'$output_name 
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
